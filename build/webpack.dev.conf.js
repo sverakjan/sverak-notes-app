a808f2a751e86788b482bdbf3aaa57fe23819b6b
Jan Svěrák <info@jansverak.cz>
2019-01-05 13:56:40 +0100

    Firebase hosting init & deploy

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -19,7 +19,7 @@ module.exports = merge(baseWebpackConfig, {
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: 'index.html',
+      template: 'build/index.html',
       inject: true
     })
   ]
